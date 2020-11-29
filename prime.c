b549b8e01f06ed7cea09bdce3ab4f78d366b606f
sana25052001 <75016219+sana25052001@users.noreply.github.com>
2020-11-29 14:06:16 +0530

    Created prime.c
    Here the program will give output as 454545 is not a prime number

---
+++ prime.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int prime(int,int);
+
+int main()
+{
+int a=454545,check;
+printf("the entered number is %d\n",a);
+check= prime(a,a/2);
+if(check==1)
+{
+printf("the number is prime");
+}
+else
+{
+    printf("the number is not prime");
+}
+    return 0;
+}
+int prime(int a,int b)
+{
+if(a==1)
+{
+    return 1;
+}
+else
+{
+    if(a%b==0)
+    {
+        return 0;
+            }
+            else 
+            {
+                return prime(a,b-1);
+            }
+}
+}
